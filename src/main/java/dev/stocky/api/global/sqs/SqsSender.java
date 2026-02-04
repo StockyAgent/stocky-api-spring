63c9423d2b93c21ddfc0d346a03109f4cbc50878
junhwan seok <seok626898@gmail.com>
2026-02-04 15:56:30 +0900

    [FEAT] 심층 리포트 SQS 연동 기반 구현

--- src/main/java/dev/stocky/api/global/sqs/SqsSender.java
+++ src/main/java/dev/stocky/api/global/sqs/SqsSender.java
@@ -1,5 +1,6 @@
 package dev.stocky.api.global.sqs;
 
+import dev.stocky.api.domain.report.dto.DeepAnalysisRequestDto;
 import dev.stocky.api.domain.report.dto.RegularAnalysisRequestDto;
 import io.awspring.cloud.sqs.operations.SqsTemplate;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,9 @@ public class SqsSender {
   @Value("${app.sqs.queue.regular-request}")
   private String regularRequestQueue;
 
+  @Value("${app.sqs.queue.deep-request}")
+  private String deepRequestQueue;
+
   // 정기 리포트 요청 전송. 매일 아침 작동
   public void sendRegularRequest(RegularAnalysisRequestDto requestDto) {
     log.info("🚀 SQS 전송 [Regular Request]: userId={}, symbols={}",
@@ -25,6 +29,15 @@ public class SqsSender {
     // 객체(DTO)를 넣으면 자동으로 JSON으로 변환되어 전송됩니다.
     sqsTemplate.send(to -> to
         .queue(regularRequestQueue)
+        .payload(requestDto));
+  }
+
+  // 심층 리포트 요청 전송
+  public void sendDeepRequest(DeepAnalysisRequestDto requestDto) {
+    log.info("🚀 SQS 전송 [Deep Request]: symbol={}", requestDto.getSymbol());
+
+    sqsTemplate.send(to -> to
+        .queue(deepRequestQueue)
         .payload(requestDto));
   }
 }
